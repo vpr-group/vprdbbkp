a01cdd01e449a0e1854348907b6744926d3a2aa7
info@vpr-group.ch <info@vpr-group.ch>
2025-04-30 15:20:48 +0200

    Cleanup tests

--- core/src/databases/mariadb/tests.rs
+++ core/src/databases/mariadb/tests.rs
@@ -1,109 +1,78 @@
-use std::env;
+#[cfg(test)]
+mod tests {
+    use crate::databases::{mariadb::MariaDB, DbAdapter};
+    use anyhow::Result;
 
-use anyhow::Result;
-use dotenv::dotenv;
-use log::LevelFilter;
+    use dotenv::dotenv;
+    use log::LevelFilter;
+    use std::env;
+    use tokio::sync::OnceCell;
 
-use crate::databases::mariadb::{tools::MariaDBTools, version::MariaDBVersion};
+    struct ConnectionOptions {
+        host: String,
+        port: u16,
+        user: String,
+        password: String,
+        db_name: String,
+    }
 
-struct ConnectionOptions {
-    host: String,
-    port: u16,
-    user: String,
-    password: String,
-    db_name: String,
-}
+    static LOGGER: OnceCell<()> = OnceCell::const_new();
 
-fn get_connection_options() -> Result<ConnectionOptions> {
-    dotenv().ok();
+    fn get_connection_options() -> Result<ConnectionOptions> {
+        dotenv().ok();
 
-    let port: u16 = env::var("MARIADB_PORT").unwrap_or("0".into()).parse()?;
+        let port: u16 = env::var("MARIADB_PORT").unwrap_or("0".into()).parse()?;
 
-    Ok(ConnectionOptions {
-        host: env::var("MARIADB_HOST").unwrap_or_default(),
-        password: env::var("MARIADB_PASSWORD").unwrap_or_default(),
-        user: env::var("MARIADB_USERNAME").unwrap_or_default(),
-        db_name: env::var("MARIADB_NAME").unwrap_or_default(),
-        port,
-    })
-}
+        Ok(ConnectionOptions {
+            host: env::var("MARIADB_HOST").unwrap_or_default(),
+            password: env::var("MARIADB_PASSWORD").unwrap_or_default(),
+            user: env::var("MARIADB_USERNAME").unwrap_or_default(),
+            db_name: env::var("MARIADB_NAME").unwrap_or_default(),
+            port,
+        })
+    }
 
-fn initialize_test() {
-    env_logger::Builder::new()
-        .filter_level(LevelFilter::Info)
-        .init();
-}
-
-#[test]
-fn test_mariadb_tools_instantiation() {
-    initialize_test();
-    let mariadb_tools = MariaDBTools::default();
-    assert_eq!(mariadb_tools.version, MariaDBVersion::V11_4);
-}
-
-#[tokio::test]
-async fn test_mariadb_version() {
-    initialize_test();
-    let options = get_connection_options().expect("Failed to get connection options");
-    let mariadb_tools = MariaDBTools::default();
-    let version = mariadb_tools
-        .get_version(
+    fn get_mariadb() -> Result<Box<dyn DbAdapter>> {
+        let options = get_connection_options()?;
+        let mariadb = MariaDB::new(
             &options.db_name,
             &options.host,
             options.port,
             &options.user,
             Some(options.password.as_str()),
-        )
-        .await
-        .expect("Unable to check MariaDB connection");
+        );
 
-    println!("{:?}", version);
-}
+        Ok(Box::new(mariadb))
+    }
 
-#[tokio::test]
-async fn test_mariadb_connection() {
-    initialize_test();
-    let options = get_connection_options().expect("Failed to get connection options");
-    let mariadb_tools = MariaDBTools::default();
-    let is_connected = mariadb_tools
-        .is_connected(
-            &options.db_name,
-            &options.host,
-            options.port,
-            &options.user,
-            Some(options.password.as_str()),
-        )
-        .await
-        .expect("Unable to check MariaDB connection");
+    async fn initialize_test() {
+        LOGGER
+            .get_or_init(|| async {
+                env_logger::Builder::new()
+                    .filter_level(LevelFilter::Info)
+                    .init();
+            })
+            .await;
+    }
 
-    assert!(is_connected);
-}
+    #[tokio::test]
+    async fn test_mariadb_is_connected() {
+        initialize_test().await;
+        let mariadb = get_mariadb().expect("Failed to construct MariaDB adapter");
+        let is_connected = mariadb
+            .is_connected()
+            .await
+            .expect("Failed to check if MariaDB is connected");
 
-#[tokio::test]
-async fn test_mariadb_dump() {
-    initialize_test();
-    let options = get_connection_options().expect("Failed to get connection options");
-    let mariadb_tools = MariaDBTools::with_detected_version(
-        &options.db_name,
-        &options.host,
-        options.port,
-        &options.user,
-        Some(options.password.as_str()),
-    )
-    .await
-    .expect("Failed to initialize MariaDBTools");
+        assert_eq!(is_connected, true);
+    }
 
-    let bytes = mariadb_tools
-        .dump(
-            &options.db_name,
-            &options.host,
-            options.port,
-            &options.user,
-            Some(options.password.as_str()),
-            None,
-        )
-        .await
-        .expect("Unable to check MariaDB connection");
+    #[tokio::test]
+    async fn test_mariadb_dump() {
+        initialize_test().await;
+        let mariadb = get_mariadb().expect("Failed to construct MariaDB adapter");
+        let bytes = mariadb.dump(None).await.expect("Unable to create a dump");
 
-    assert!(bytes.len() > 0);
+        assert!(bytes.len() > 0);
+    }
 }
