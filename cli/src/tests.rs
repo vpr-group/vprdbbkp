f2d68b84cb12e644f30844ecce91bb7920ee3e5a
info@vpr-group.ch <info@vpr-group.ch>
2025-05-14 14:42:47 +0200

    Remove one test

--- cli/src/tests.rs
+++ cli/src/tests.rs
@@ -29,20 +29,6 @@ mod tests {
         })
     }
 
-    fn get_mariadb_connection_options() -> Result<ConnectionOptions> {
-        dotenv().ok();
-
-        let port: u16 = env::var("MARIADB_PORT").unwrap_or("0".into()).parse()?;
-
-        Ok(ConnectionOptions {
-            host: env::var("MARIADB_HOST").unwrap_or_default(),
-            password: env::var("MARIADB_PASSWORD").unwrap_or_default(),
-            user: env::var("MARIADB_USERNAME").unwrap_or_default(),
-            db_name: env::var("MARIADB_NAME").unwrap_or_default(),
-            port,
-        })
-    }
-
     fn extract_filename(text: &str) -> Option<String> {
         let re = Regex::new(r"\s+([a-zA-Z0-9_-]+-\d{4}-\d{2}-\d{2}-\d{6}-[a-z0-9]+\.gz)").unwrap();
 
@@ -123,65 +109,5 @@ mod tests {
             .expect("Faled to execute cleanup command");
 
         assert!(String::from_utf8_lossy(&cleanup_output.stdout).contains("1 Entries deleted"));
-    }
-
-    #[test]
-    fn test_02_backup_list_restore_mariadb() {
-        let options = get_mariadb_connection_options().expect("Failed to get mariadb options");
-        let mut cmd = Command::cargo_bin("cli").expect("Failed to get cli command");
-
-        let backup_output = cmd
-            .arg("backup")
-            .arg(format!("--source-type=mariadb"))
-            .arg(format!("--database={}", options.db_name))
-            .arg(format!("--host={}", options.host))
-            .arg(format!("--username={}", options.user))
-            .arg(format!("--password={}", options.password))
-            .arg(format!("--port={}", options.port))
-            .arg(format!("--storage-type=local"))
-            .arg(format!("--root=./test-backups-mariadb"))
-            .output()
-            .expect("Failed to execute command");
-
-        assert!(backup_output.status.success());
-        assert!(String::from_utf8_lossy(&backup_output.stdout)
-            .contains("Backup completed successfully"));
-
-        cmd = Command::cargo_bin("cli").expect("Failed to get cli command");
-
-        let list_output = cmd
-            .arg("list")
-            .arg("--latest-only")
-            .arg(format!("--storage-type=local"))
-            .arg(format!("--root=./test-backups-mariadb"))
-            .output()
-            .expect("Failed to execute command");
-
-        let string_output = String::from_utf8_lossy(&list_output.stdout);
-        let file_name = extract_filename(&string_output.to_owned());
-
-        assert_eq!(file_name.is_some(), true);
-
-        let file_name = file_name.unwrap();
-
-        cmd = Command::cargo_bin("cli").expect("Failed to get cli command");
-
-        let restore_output = cmd
-            .arg("restore")
-            .arg(format!("--filename={}", file_name))
-            .arg(format!("--drop-database=true"))
-            .arg(format!("--source-type=mariadb"))
-            .arg(format!("--database={}", options.db_name))
-            .arg(format!("--host={}", options.host))
-            .arg(format!("--username={}", options.user))
-            .arg(format!("--password={}", options.password))
-            .arg(format!("--port={}", options.port))
-            .arg(format!("--storage-type=local"))
-            .arg(format!("--root=./test-backups-mariadb"))
-            .output()
-            .expect("Failed to execute command");
-
-        assert!(String::from_utf8_lossy(&restore_output.stdout)
-            .contains("Restore completed successfully"));
     }
 }
